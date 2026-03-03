013e6519cc58058050aa0f53dceb96cacf236c68
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-04 01:20:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int n = nums.length;  
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
+            i++;   
+        }
+        return nums;
+    }
+}
