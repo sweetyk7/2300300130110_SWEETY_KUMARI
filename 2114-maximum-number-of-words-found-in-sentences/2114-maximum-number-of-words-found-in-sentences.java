f0541513fc28033518bdd3c73bacde06b19fcc21
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-04 03:04:42 +0530

    Time: 4 ms (42.34%), Space: 44.5 MB (69.09%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,16 +1,16 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
         int ans = 0;
-        for(int i=0;i<sentences.length;i++){
-            int count = 0;
-            for(int j=0;j<sentences[i].length();j++){
+        for(int i=0;i<sentences.length;i++){ // the reason to do not take sentences.length() -> because all sentences are seperated["....","...","...."] and we easily find this sentences with their index
+            int count = 0;  // beacause we count spaces under sentences and also we count all spaces of different sentence so we have to initialize count = 0 (taki her sentenses ke liye count 0 se le paye nhi to dusre sentence ka count bhi usme add ho jayega)
+            for(int j=0;j<sentences[i].length();j++){ // now we check characters in each sentence
                 char ch = sentences[i].charAt(j);
                 if(ch == ' '){
                     count++;
                 }
             }
-            ans = Math.max(ans,count+1);
+            ans = Math.max(ans,count); // now we compare all words of their each sentence
         }
-        return ans;
+        return ans+1;  // words = spaces(count) + 1
     }
 }
