a767f435dd6f5232028b4cc8dfc7462760fc256d
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-05 01:49:55 +0530

    Time: 2 ms (59.79%), Space: 47.7 MB (27.7%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -15,7 +15,7 @@ class Solution {
                     right--;
                 }   
             }
-            if(left == right){ // after checking palindrome,now we return palindrom word so i use this condition (because in last,Ex-> aba,left=b & right =b )      
+            if(left >= right){ // after checking palindrome,now we have to return palindrom word so i use this condition (because in last: 1.for odd length(aba),after checking a & a -> left=b(i=2) & right=b(i=2),so left==right and also in last:2.for even length(abba),after checking ab & ba -> left=b(i=2) &  right=b(i=1) so left>right (rememeber for even len after checking left(i=0 & i=1) and right(i=4 & i=3),after that left=b(i=2) &  right=b(i=1))    
                 return words[i];  // after confirm palindrom then return the palindrom words       
             }
         }
