af3cee485608e862f1cc073025583f085cd917b2
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-05 22:30:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int xorOperation(int n, int start) {
-        int sum = start+2*0;
-        for(int i=1;i<n;i++){
-            int val = start + 2 * i;
+        int sum = start+2*0; // because we do bitwise opertion at starting index(i=0)
+        for(int i=1;i<n;i++){  //now we take xor operation with next element,i=1 to i<n
+            int val = start + 2 * i; 
             sum = sum ^ val;
         }
         return sum;
