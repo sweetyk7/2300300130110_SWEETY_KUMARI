4a694392ba83e61ffe167f81cc6bf84fafe6fe9d
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-06 02:09:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -3,13 +3,13 @@ class Solution {
         int n = nums.length;
         int sign = 1;
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                    return 0;
+            if(nums[i] == 0){
+                return 0;
             }
-            else if(nums[i]<0){
-                sign=-sign;  // if nums[2] = -5 then sign = -sign means -1 and then check for next if next nums[3] = 2 then sign means same -1(because -val * +val = -val) and if nums[3]=-2 then sign =-sign means -(-1)=1
+            else if(nums[i] < 0){
+                sign=-sign;
             }
         }
-        return sign;       
+        return sign;
     }
 }
