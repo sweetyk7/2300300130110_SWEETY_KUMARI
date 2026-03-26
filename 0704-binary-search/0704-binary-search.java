1653765266318510d52e4a64311a6d51a356b61d
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-26 14:20:21 +0530

    Time: 0 ms (100%), Space: 48.3 MB (63.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -21,19 +21,17 @@
 // with recursion
 class Solution {
     public int search(int[] nums, int target){
-        return solve(nums, 0, nums.length - 1, target);
+        return Solve( 0,nums.length-1,target,nums);
     }
-    public int solve(int[] nums, int left, int right, int target) {
-            if(left > right) return -1;
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target) return mid;
-            elseif(nums[mid]<target){
-                Solve(left,mid+1,target,nums);
-            }
-            else{
-                Solve(mid+1,right,target,nums);
-            }
+    public int Solve(int left,int right,int target,int[] nums) {
+        if(left > right) return -1;
+        int mid = left + (right-left)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target){
+            return Solve(left,mid-1,target,nums);
         }
-        return -1;
+        else{
+            return Solve(mid+1,right,target,nums);
+        }
     }
 }
