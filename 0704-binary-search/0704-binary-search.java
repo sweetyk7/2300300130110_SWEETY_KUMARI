c5f79af9347499a98c5fc5bbd534b11ac86a6df7
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-26 10:36:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,16 +1,37 @@
+// with recursion
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int n = nums.length; 
+//         int left=0;
+//         int right=n-1;
+//         while(left<=right){
+//             int mid = left + (right-left)/2;
+//             if(nums[mid]==target) return mid;
+//             if(nums[mid]<target){
+//                 left=mid+1;
+//             }
+//             else{
+//                 right=mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+// with recursion
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length; 
-        int left=0;
-        int right=n-1;
-        while(left<=right){
+    public int search(int[] nums, int target){
+        return solve(nums, 0, nums.length - 1, target);
+    }
+    public int solve(int[] nums, int left, int right, int target) {
+            if(left > right) return -1;
             int mid = left + (right-left)/2;
             if(nums[mid]==target) return mid;
-            if(nums[mid]<target){
-                left=mid+1;
+            elseif(nums[mid]<target){
+                Solve(left,mid+1,target,nums);
             }
             else{
-                right=mid-1;
+                Solve(mid+1,right,target,nums);
             }
         }
         return -1;
