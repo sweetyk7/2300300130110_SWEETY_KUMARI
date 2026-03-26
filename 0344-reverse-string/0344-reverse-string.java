2c0ba9c5683580566755684a2c5db5d82790233a
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-03-26 13:53:37 +0530

    Time: 1 ms (22.74%), Space: 48.9 MB (5.99%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,14 +1,30 @@
+//without recursion
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int i = 0;
+//         int j = s.length-1;
+//         if(s.length<=0) return ;
+//         while(i<j){
+//             char temp = s[i];
+//             s[i]=s[j];
+//             s[j]=temp;
+//             i++;
+//             j--;
+//         }
+//     }
+// }
+
+
+//without recursion
 class Solution {
     public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length-1;
-        if(s.length<=0) return ;
-        while(i<j){
-            char temp = s[i];
-            s[i]=s[j];
-            s[j]=temp;
-            i++;
-            j--;
-        }
+        Solve(s,0,s.length-1);
+    }    
+    public void Solve(char[] s, int i, int j) {
+        if(i>=j) return ;
+        char temp = s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        Solve(s,i+1,j-1);
     }
 }
