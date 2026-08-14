8d73c15d5fdc7996f37e9100a295da4ee50e8eb7
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-08-15 05:00:21 +0530

    Time: 3 ms (94.79%), Space: 42.6 MB (61.97%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -3,7 +3,7 @@ class Solution {
         int sum = 0;
         for(int i=1;i<=n;i++){
             if(i%3==0 || i%5==0 || i%7==0){
-                sum += i;
+                sum = sum+i;
             }
         }
         return sum;
