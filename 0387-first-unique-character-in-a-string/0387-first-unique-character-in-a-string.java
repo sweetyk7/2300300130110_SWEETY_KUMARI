480d249f99b51dacad606289c603dfb08f90c680
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-08-14 23:53:53 +0530

    Time: 38 ms (8.06%), Space: 46.9 MB (57.94%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -8,15 +8,15 @@ class Solution {
                 map.put(ch,1);
             }
             else{
-                map.put(ch,map.get(ch) + 1);
+                map.put(ch,map.get(ch)+1);
             }
         }
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(map.get(ch) == 1){
+            if(map.get(ch)==1){
                 return i;
             }
         }
-        return -1;
-    }        
+        return -1;    
+    }
 }
