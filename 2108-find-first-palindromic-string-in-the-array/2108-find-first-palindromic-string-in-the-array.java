f5430ab6a2ecdd6a5efd519556d7508811d474ab
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-08-15 03:56:57 +0530

    Time: 2 ms (59.11%), Space: 47.4 MB (86.48%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,22 +1,19 @@
 class Solution {
     public String firstPalindrome(String[] words) {
-
-        for(int i = 0; i < words.length; i++){
-
+        for(int i=0;i<words.length;i++){
             int left = 0;
             int right = words[i].length() - 1;
-
-            while(left < right){
-                if(words[i].charAt(left) != words[i].charAt(right)){
-                    break;
-                } 
-                else{
+            while(left<right){
+                if(words[i].charAt(left) == words[i].charAt(right)){
                     left++;
                     right--;
-                }   
+                }
+                else{
+                    break;
+                }
             }
-            if(left >= right){ // after checking palindrome,now we have to return palindrom word so i use this condition (because in last: 1.for odd length(aba),after checking a & a -> left=b(i=2) & right=b(i=2),so left==right and also in last:2.for even length(abba),after checking ab & ba -> left=b(i=2) &  right=b(i=1) so left>right (rememeber for even len after checking left(i=0 & i=1) and right(i=4 & i=3),after that left=b(i=2) &  right=b(i=1))    
-                return words[i];  // after confirm palindrom then return the palindrom words       
+            if(left>=right){
+                return words[i];
             }
         }
         return "";
