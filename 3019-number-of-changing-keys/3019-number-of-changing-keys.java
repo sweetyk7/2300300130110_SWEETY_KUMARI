3b67b272fc1612c7ed48d2ea88d6d5ac11139476
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-08-15 07:29:33 +0530

    Time: 1 ms (100%), Space: 43.5 MB (48.6%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.java
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countKeyChanges(String s) {
-        s = s.toLowerCase();
+        s=s.toLowerCase();
         int count = 0;
         for(int i=0;i<s.length()-1;i++){
             if(s.charAt(i) != s.charAt(i+1)){
