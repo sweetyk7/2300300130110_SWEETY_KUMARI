93a4d59ef6db6982451269ed1cd9f560bd89343f
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-08-16 02:31:06 +0530

    Time: 1 ms (93.9%), Space: 45.7 MB (20.21%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean canAliceWin(int[] nums) {
-        int n = nums.length;
-        int singleDigitSum = 0;
-        int doubleDigitSum = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=0 && nums[i]<=9){
-                singleDigitSum = singleDigitSum + nums[i];
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10){
+                sum1 = sum1 + nums[i]; 
             }
-            else if(nums[i]>=10 && nums[i]<=99){
-                doubleDigitSum = doubleDigitSum + nums[i];
+            else if(nums[i]>=10 && nums[i]<100){
+                sum2 = sum2 + nums[i];
             }
         }
-        if(singleDigitSum > doubleDigitSum || singleDigitSum < doubleDigitSum){
-            return true;
+        if((sum1 > sum2) || (sum1 < sum2)){
+            return true; 
         }
-        return false;
+        else{
+            return false;
+        }
     }
 }
