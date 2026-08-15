a19782fa4a3a0e85da20891660d22c04692558cf
Sweety Kumari <sweety.kumari56879@gmail.com>
2026-08-16 00:06:53 +0530

    Time: 10 ms (35.91%), Space: 47.7 MB (25.3%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            nums[i] = nums[i]*nums[i]; //We can't write(nums[i]^2) beacause ^ is bitwise operater not power
+        for(int i=0;i<nums.length;i++){
+            nums[i] = (nums[i] * nums[i]);
         }
-        Arrays.sort(nums); //default in increasing order
+        Arrays.sort(nums);
         return nums;
     }
 }
